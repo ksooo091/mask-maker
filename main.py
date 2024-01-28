b97ab748ce4a7fd7e922e4ab5c28ce9d9c03513a
KimGilSu <hwanrim00@gmail.com>
2024-01-29 02:24:50 +0900

    [feat] gcs 업로드 추가

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import configparser as parser
 import json
 from json import dumps
 import time
-
+from google.cloud import storage
 
 properties = parser.ConfigParser()
 properties.read('./config/config.ini')
@@ -17,6 +17,13 @@ kafka_config = properties['Kafka']
 bootstrap_servers = kafka_config['bootstrap_servers']
 sasl_plain_username = kafka_config['sasl_plain_username']
 sasl_plain_password = kafka_config['sasl_plain_password']
+gcp_config = properties['gcp']
+project_id = gcp_config['project_id']
+gcs_bucket = gcp_config['gcs_bucket']
+
+def gcs_connection():
+    client = storage.Client(project="study-406403")
+    return client
 
 
 def s3_connection():
@@ -37,16 +44,28 @@ def s3_connection():
 s3 = s3_connection()
 
 
-def image_convert(originKey, userMail):
+def image_convert(origin_key, mail):
     now = time.time()
     now = int(now)
     now = str(now)
 
-    origin_key = originKey
     image_bytes = s3.get_object(Bucket=s3_bucket, Key=origin_key)["Body"].read()
-    image_key = (origin_key.split('/')[1]).split('.')[0]+userMail+now+".png"
+    image_key = (origin_key.split('/')[1]).split('.')[0] + mail + now + ".png"
     s3.put_object(Bucket=s3_bucket, Key="mask/" + image_key, Body=remove(image_bytes, only_mask=True))
-    print("Converted and put Success. User : " + userMail )
+    print("Converted and put Success. User : " + mail)
+    return image_key
+
+
+def image_convert_gcs(origin_key, mail):
+    now = time.time()
+    now = int(now)
+    now = str(now)
+    bucket = gcs_connection().bucket(gcs_bucket)
+    image_bytes = bucket.blob(origin_key).download_as_string()
+    image_key = (origin_key.split('/')[1]).split('.')[0] + mail + now + ".png"
+    blob = bucket.blob(image_key)
+    blob.upload_from_string(remove(image_bytes, only_mask=True))
+    print("Converted and put Success. User : " + mail)
     return image_key
 
 
@@ -74,12 +93,13 @@ producer = KafkaProducer(
 # 카프카 메시지 처리
 for message in consumer:
     message = message.value.decode('utf-8')
-    user_mail = json.loads(message).pop('user_mail') 
-    producer.send('maskImage', value=json.dumps({"prompt":json.loads(message).pop('prompt'),
-                                                 "mask_key": image_convert(json.loads(message).pop('origin_key'),user_mail),
+    user_mail = json.loads(message).pop('user_mail')
+    producer.send('maskImage', value=json.dumps({"prompt": json.loads(message).pop('prompt'),
+                                                 "mask_key": image_convert(json.loads(message).pop('origin_key'),
+                                                                           user_mail),
                                                  "user_mail": user_mail,
                                                  "origin_key": json.loads(message).pop('origin_key')
-                                                }))
+                                                 }))
 
 producer.close()
 consumer.close()
